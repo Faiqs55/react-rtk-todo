22be13a9a95737800e625960c6815e705b32ecf0
Faiq Sindhu <sindhufaiq555@gmail.com>
2024-09-12 02:14:04 +0500

    done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useSelector } from "react-redux"
+import AddTodo from "./components/addTodo"
+import Todos from "./components/Todos";
+
+function App() {
+
+  const todos = useSelector(state => state.todos);
+
+
+  return (
+    <div className="text-center w-[70%] mx-auto">
+     <AddTodo/>
+     <Todos todos={todos} />
+    </div>
+  )
+}
+
+export default App
