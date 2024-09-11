7be640933b8d24b85a156f209f87f082cb0f1b37
Faiq Sindhu <sindhufaiq555@gmail.com>
2024-09-11 21:20:18 +0000

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux"
-import AddTodo from "./components/addTodo"
+import AddTodo from "./components/AddTodo"
 import Todos from "./components/Todos";
 
 function App() {
